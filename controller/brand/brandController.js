61426aa22434b008d742d4fb3d48dfba4bd8b6dc
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-06-12 11:36:52 +0600

    created a contoller for update brand data

--- controller/brand/brandController.js
+++ controller/brand/brandController.js
@@ -1,4 +1,6 @@
 const Brand = require("../../modal/brandSchema");
+const fs = require("fs");
+const path = require("path");
 
 //get all brands
 const getAllBrands = async (req, res, next) => {
@@ -57,28 +59,55 @@ const addBrand = async (req, res, next) => {
 //update a brand
 const updateBrand = async (req, res, next) => {
   try {
-    const { status } = req.body;
-    const { brandid } = req.params;
-    const updateData = await Brand.findByIdAndUpdate(
-      { _id: brandid },
-      {
-        $set: {
-          status: status,
-        },
-      },
-      {
-        new: true,
+    const { brandId } = req.params;
+    const { name, description, status } = req.body;
+    const { filename } = req.file || {};
+
+    // Find the brand by brandId
+    const brand = await Brand.findById(brandId);
+
+    if (!brand) {
+      return res.status(404).json({ error: "Brand not found" });
+    }
+
+    // Update the name if provided
+    if (name) {
+      brand.name = name;
+    }
+
+    // Update the description if provided
+    if (description) {
+      brand.description = description;
+    }
+
+    // Update the status if provided
+    if (status !== undefined) {
+      brand.status = status;
+    }
+
+    // Check if a new picture is provided
+    if (filename) {
+      // Delete the old picture from the local folder
+      if (brand.picture) {
+        const oldPicturePath = path.join("./uploads", brand.picture);
+        fs.unlinkSync(oldPicturePath);
       }
-    );
-    res.status(200).json({
-      brand: updateData,
-    });
+
+      // Update the picture filename
+      brand.picture = filename;
+    }
+
+    // Save the updated brand
+    const updatedBrand = await brand.save();
+
+    res.status(200).json(updatedBrand);
   } catch (err) {
     res.status(500).json({
       message: err.message,
     });
   }
 };
+
 //update a brand status
 const updateBrandStatus = async (req, res, next) => {
   try {
