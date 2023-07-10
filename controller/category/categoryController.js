8b8c06e418427bb8b0e55a225c73bd66bc3a89c2
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-07-10 14:24:33 +0600

    created a addcategory controller

--- controller/category/categoryController.js
+++ controller/category/categoryController.js
@@ -1,9 +1,42 @@
-
+const Category = require("../../modal/categorySchema");
 
 const getCategory = async (req, res, next) => {
-    
-}
+  try {
+    const categories = await Category.find({});
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const addCategory = async (req, res, next) => {
+  try {
+    const { name, brandInfo } = req.body;
+    const { filename } = req.file || {};
+    const category = new Category({
+      name,
+      status: true,
+      brandInfo,
+      picture: filename,
+    });
+    const result = await category.save();
+    if (result._id) {
+      res.status(200).json(result);
+    } else {
+      res.status(500).json({
+        message: "Something Was Wrong!",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 module.exports = {
-    getCategory
-}
+  getCategory,
+  addCategory,
+};
